9027f83eb428f7a9ba7a5a1c1f4f52bfec6761b2
serj053 <serj053@yandex.ru>
2024-07-27 21:09:01 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,15 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+students_list = list(students)
+students_list.sort()
+print(students_list)
+dict = {
+students_list[0]:grades[0],
+students_list[1]:grades[1],
+students_list[2]:grades[2],
+students_list[3]:grades[3],
+students_list[4]:grades[4]
+        }
+print(dict)
