9d84663c13f9998c7e9eeb8e2df9410f86b313cb
serj053 <serj053@yandex.ru>
2024-07-29 00:05:32 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,10 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while True:
-    if my_list[i] >= 0:  # если считать 0 положительным числом.
+    if my_list[i] == 0:
+        i = i + 1
+    elif my_list[i] > 0:
         print(my_list[i])
         i = i + 1
-        continue
     else:
         break
-
-# elif my_list[i] < 0 or my_list[i] == len(my_list): - избыточный код
