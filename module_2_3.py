9d09a3ba4840775dd1d5b58aaaad93160b55bc22
serj053 <serj053@yandex.ru>
2024-07-29 00:20:14 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,1 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-i = 0
-while True:
-    if my_list[i] == 0:
-        i = i + 1
-    elif my_list[i] > 0:
-        print(my_list[i])
-        i = i + 1
-    else:
-        break
+
