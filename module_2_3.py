fece4f9827050a66452504f8b3ec21057d6bff4c
serj053 <serj053@yandex.ru>
2024-07-29 00:22:37 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,1 +1,14 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while True:
+    if i == len(my_list):
+        break
+    if my_list[i] == 0:
+        i = i + 1
+        continue
+    elif my_list[i] > 0:
+        print(my_list[i])
+        i = i + 1
+    else:
+        break
 
