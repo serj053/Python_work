68b0edaf1bf7b7a071736b009501496b4550bcf6
serj053 <serj053@yandex.ru>
2024-07-29 18:40:58 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(numbers[::])
+primes = []
+not_primes = []
+for k in range(len(numbers) + 1):
+    if k == 0 or k == 1:
+        continue
+    n = 0
+    nn = len(numbers[0:k])
+    for j in range(nn):
+        if k % (j + 1) == 0:
+            n += 1
+    if n == 2:
+        primes.append(k)
+    else:
+        not_primes.append(k)
+
+print(primes)
+print(not_primes)
