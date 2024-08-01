a1535ec59f309f586d07128a1b35dbe2ce608f29
serj053 <serj053@yandex.ru>
2024-08-01 08:36:50 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -5,7 +5,6 @@ for i in range(1, x):
     for j in range(i, x):
         if x % (i + j) == 0:
             m1 = str(i) + str(j)
-            print(i, j)
             m2.append(m1)
 
 for i in range(1,len(m2)):
