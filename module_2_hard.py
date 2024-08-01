24c8b3c01c8374d41ff9d9a2dfb3f7973551bbe1
serj053 <serj053@yandex.ru>
2024-08-01 08:28:38 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,14 @@
+x = 4
+m2 = []
+m1 = []
+for i in range(1, x):
+    for j in range(i, x):
+        if x % (i + j) == 0:
+            m1 = str(i) + str(j)
+            print(i, j)
+            m2.append(m1)
+
+for i in range(1,len(m2)):
+    m2[0] += m2[i]
+
+print(m2[0])
